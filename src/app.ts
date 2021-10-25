9c6c98e7a49361c2bc0e345ad8b444e5a539efb1
esellers25 <erin.e.sellers@gmail.com>
2021-10-25 11:38:57 -0400

    inheritance plus getters and setters

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 class Department {
-    private employees: string[] = [];
+    protected employees: string[] = [];
     
     //this is the faster way to initialize properties in Typescript
     //note that you do have to explicitly type public or private here 
@@ -21,6 +21,39 @@ class Department {
     }
 }
 
+class ITDepartment extends Department {
+    private firstAdmin: string; 
+    
+    constructor(id: number, private admins: string[]){
+        super('IT', id); 
+        this.firstAdmin = this.admins[0]; 
+        this.addAdmins(admins); 
+    }
+
+    addAdmins(adminList: string[]){
+        adminList.forEach(admin => this.addEmployee(admin)); 
+    }
+
+    isAdmin(name: string){
+        if(this.admins.filter(admin => admin === name).length === 1) {
+            return true; 
+        } else return false; 
+    }
+
+    get primaryAdmin() {
+        if(this.firstAdmin){
+            return this.firstAdmin; 
+        }
+        throw new Error('No admins found.')
+    }
+
+    set primaryAdmin(name: string) {
+        this.admins.unshift(name); 
+        this.addEmployee(name);
+        this.firstAdmin = name; 
+    }
+}
+
 const accounting = new Department("Accounting", 15); 
 accounting.addEmployee("Stephanie");
 accounting.addEmployee("Natalie");
@@ -29,6 +62,15 @@ accounting.printEmployeeInfo();
 
 accounting.name = "Business Finances";
 accounting.describe(); 
+
+const itDept = new ITDepartment(21, ['Erin', 'Jon']); 
+itDept.describe();
+itDept.printEmployeeInfo(); 
+console.log(itDept.primaryAdmin);
+itDept.primaryAdmin = 'Tammy';
+console.log(itDept.primaryAdmin);
+
+ 
 
 // const accountingCopy = { name: "duplicate", describe: accounting.describe };
 
