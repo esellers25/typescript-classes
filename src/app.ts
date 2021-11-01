6bf519aed58e9ff14419f477e5754fcb92aa69bc
esellers25 <erin.e.sellers@gmail.com>
2021-11-01 11:39:50 -0400

    decorator return types

--- src/app.ts
+++ src/app.ts
@@ -16,12 +16,18 @@ function Logger(comment: string){
 
 function WithTemplate(template: string, hookId: string){
     console.log('template factory')
-    return function(constructor: any){
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor(); 
-        if (hookEl){
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = p.name
+    // the line below says that our generic type T is a type of object that will take in any number of arguments
+    // it will return an object that contains a key of name with a string value 
+    return function<T extends {new(...args: any[]): {name: string}}>(originalConstructor: T){
+        return class extends originalConstructor {
+            constructor(..._: any[]){
+                super();
+                const hookEl = document.getElementById(hookId);
+                if (hookEl){
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = this.name
+                }
+            }
         }
     }
 }
@@ -42,11 +48,40 @@ console.log(p1);
 
 // --- 
 
-class Product {
-    constructor(public title: string, public _price: number){
+function Log(target: any, propertyName: string){
+    console.log('Property decorator');
+    console.log(target, propertyName);
+}
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor){
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number){
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+
+class Product {
+    @Log
+    title: string; 
+    constructor(t: string, public _price: number){
+        this.title = t; 
     }
 
+    @Log2
     set price(val: number){
         if(val > 0){
             this._price = val; 
@@ -55,7 +90,41 @@ class Product {
         }
     }
 
-    getPriceWithTax(tax:number){
+    @Log3
+    getPriceWithTax(@Log4 tax:number){
         return this._price * (1 + tax)
     }
 }
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor){
+    // description.value gives us access to the original method 
+    const originalMethod = descriptor.value; 
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false, 
+        // the below line allows us to do extra work before we execute the function
+        // it's like having a value property that has extra logic that runs before the value is returned 
+        // here, this refers to whatever is responsible for triggering this get method 
+        // it will always refer to the object on which we call the getter method 
+        // it's a different way of binding 'this' to the instance object 
+        get() {
+            const boundFn = originalMethod.bind(this); 
+            return boundFn;
+        }
+    };
+    return adjDescriptor; 
+}
+
+class Printer {
+    message = 'This works';
+
+    @Autobind
+    showMessage(){
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage)
